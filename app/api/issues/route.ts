81a14b37e1c9d046b03a83b85c525d28aa8c2345
russllist <russell.cronin@gmail.com>
2025-05-09 12:07:57 -0600

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,15 +5,15 @@ import {z} from 'zod';
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, "Title is required").max(255),
+    description: z.string().min(1, "Description is required")
 });
 
-export async function POST(request: NextRequest) {              //Function called POST that takes the argument 'request' of type 'NextRequest'
-    const body = await request.json();                          //Returns promise. A json object when successful?
-    const validation = createIssueSchema.safeParse(body);
+export async function POST(request: NextRequest) {              // Function called POST that takes the argument 'request' of type 'NextRequest'
+    const body = await request.json();                          // Returns promise. A json object when successful?
+    const validation = createIssueSchema.safeParse(body);       // zodObject.zodMethod(argument) creates SafeParseReturnType
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.format(), {status: 400})
 
     const newIssue = await prisma.issue.create({
         data: {title: body.title, description: body.description}
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from 'react';
-import { Button, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -16,28 +16,40 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>(); // useForm hook from RHF, with shape defined by interface, destructured and saved as variable
+  const [error, setError] = useState('');
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data); // Make sure database server is running! Else get axios error
-        router.push('/issues');
-      })}
-    >
-      <TextField.Root
-        placeholder="Title"
-        {...register('title')}
-      ></TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues', data); // Make sure database server is running! Else get axios error
+            router.push('/issues');
+          } catch (error) {
+            setError('An unexpected error occurred.');
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register('title')}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
