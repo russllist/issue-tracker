46bb1ae135f3fca5c9db07ca1a7ef9011b104053
russllist <russell.cronin@gmail.com>
2025-05-09 13:23:45 -0600

    Implement client-side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,13 +1,8 @@
 // This is the API that allows this app to write to and and read from the MySQL database via Prisma
 
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod';
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-    title: z.string().min(1, "Title is required").max(255),
-    description: z.string().min(1, "Description is required")
-});
+import { createIssueSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {              // Function called POST that takes the argument 'request' of type 'NextRequest'
     const body = await request.json();                          // Returns promise. A json object when successful?
